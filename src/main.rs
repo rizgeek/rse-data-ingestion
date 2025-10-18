55d929988ea51c0e78e39076d3544c6661d971c0
Muhammad Rizki <rizgeek@gmail.com>
2025-10-18 15:52:04 +0700

    feat : set dynamic path, make constant buffer size, setup print data

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use data_ingestion::prepare_data;
 
 fn main() {
-    if let Err(e) = prepare_data::print_data() {
+    if let Err(e) = prepare_data::print_data("fulldata_augmented_product_nd.ndjson") {
         eprint!("Error {}", e);
     }
 }
--- src/prepare_data.rs
+++ src/prepare_data.rs
@@ -1,40 +1,36 @@
-use std::{fs::File, io::BufReader};
-use serde_json::{Error, Value};
+use std::{fs::File, io::{BufReader}};
+use serde_json::Deserializer;
 use crate::odt::ProductData;
 
 
-fn buffer_reader_from_file(path: String, capacity: usize)-> Result<BufReader<File>, std::io::Error> {
+const DEFAULT_BUF_SIZE: usize = 8 * 1024;
+
+fn make_reader(path: &str, capacity: usize)-> Result<BufReader<File>, std::io::Error> {
     let file = File::open(path)?;
     let reader = BufReader::with_capacity(capacity, file);
 
     Ok(reader)
 }
 
-fn deserialize_json_from_reader(reader: BufReader<File>) -> Result<Value, serde_json::Error> {
-    let value: Value = serde_json::from_reader(reader)?;
-    Ok(value)
+fn read_ndjson (reader: BufReader<File>) -> impl Iterator<Item = Result<ProductData, serde_json::Error>> {
+    let stream = Deserializer::from_reader(reader).into_iter::<ProductData>();
+    stream
 }
 
-fn parse_product(item: Value) -> Result<ProductData, Error> {
-    serde_json::from_value::<ProductData>(item)
+fn display_products(product: &ProductData) {
+    println!("{:?}", product)
 }
 
-fn display_products(products: &[ProductData]) {
-    for product in products {
-        println!("{:?}", product)
-    }
-}
+pub fn print_data(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let reader = make_reader(path, DEFAULT_BUF_SIZE)?;
+    let stream = read_ndjson(reader);
+    
 
-pub fn print_data() -> Result<(), Box<dyn std::error::Error>> {
-    let reader = buffer_reader_from_file("sample_augmented_product.json".to_string(), 8 * 1024)?;
-    let value = deserialize_json_from_reader(reader)?;
-
-    if let Value::Array(items) = value {
-        let products: Result<Vec<ProductData>, Error> = items.into_iter()
-            .map(parse_product)
-            .collect();
-
-        display_products(&products?);
+    for (i, value) in stream.enumerate() {
+        match value {
+            Ok(product) => display_products(&product),
+            Err(e) => eprintln!("Error di baris {}: {:?}", i + 1, e),
+        }
     }
 
     Ok(())
